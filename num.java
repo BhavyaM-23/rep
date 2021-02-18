1c8c7824176f3ad788045771280d0d9b214ccac9
Bhavya <mbsbhavya@gmail.com>
2021-02-18 20:07:49 +0530

    commited 2 files

---
+++ num.java
@@ -0,0 +1,11 @@
+class num
+{
+	public static void main(String args[])
+	{
+		System.out.println("printing 10 natural numbers");
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
